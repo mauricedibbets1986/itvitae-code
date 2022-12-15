4ed25dbda91d24dfdfb2bbaca0aef1de2d09c87a
Maurice Dibbets <mauricedibbets1986@gmail.com>
2022-12-15 11:18:00 +0100

    les 11 uitwerking miniopdracht 1

--- les 11 SQL DQL DML/sql-les.sql
+++ les 11 SQL DQL DML/sql-les.sql
@@ -1,3 +1,5 @@
+-- WERKEN MET DE FABRIEK DB TIJDENS DE LES
+
 -- SELECT FROM
 SELECT * FROM klant;
 SELECT naam, plaats FROM klant;
@@ -15,3 +17,21 @@ VALUES (99999, 'Kees', 'straatnaam', '''s-Hertogenbosch');
 select * from klant
 WHERE plaats = '''s-Hertogenbosch';
 
+-- SubOpdracht 1
+-- 1 Zoek het klantnr van klant met de naam Staal uit Amsterdam
+select * from klant
+WHERE naam = 'Staal'; 
+-- 122
+
+-- 2 Hoeveel bestellingen heeft Staal gedaan?
+select * from bestelling
+WHERE klantnr = 122;
+-- 1 bestelling betstnr 124
+
+-- 3 Welke artikelen heeft Staal besteld op 2003-05-04?
+select * from besteldartikel
+WHERE bestnr = 124;
+-- artnrs 122 en 124
+select * from artikel
+WHERE artnt = 122 OR artnr = 124;
+-- high light pen en nietmachine
