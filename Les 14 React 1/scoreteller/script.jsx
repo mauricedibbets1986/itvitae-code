ebe98227e1ac8fe27c517a764682cc5741d6fd85
Maurice Dibbets <mauricedibbets1986@gmail.com>
2023-01-09 12:27:57 +0100

    les 14 code tm lunch

--- Les 14 React 1/scoreteller/script.jsx
+++ Les 14 React 1/scoreteller/script.jsx
@@ -1,26 +1,26 @@
-const Header = () => {
+const Header = (props) => {
     return (
         <header id="test">
-            <h1>Scoreboard</h1>
-            <span className="stats">Players: 1</span>
+            <h1>{props.title}</h1>
+            <span className="stats">Players: {props.totalPlayers}</span>
         </header>
     )
 };
 
-const Player = () => {
+const Player = (props) => {
     return (
         <div className="player">
-            <span className="player-name">Maurice</span>
-            <Counter />
+            <span className="player-name">{props.name}</span>
+            <Counter score={props.score}/>
         </div>
     )
 }
 
-const Counter = () => {
+const Counter = (props) => {
     return (
         <div className="counter">
             <button className="counter-action decrement"> - </button>
-            <span className="counter-score">35</span>
+            <span className="counter-score">{props.score}</span>
             <button className="counter-action increment"> + </button>
         </div>
     )
@@ -29,10 +29,12 @@ const Counter = () => {
 const App = () => {
     return (
         <div className="scoreboard">
-            <Header />
+            <Header title="Scoreboard" totalPlayers={3}/>
 
             {/* Player list */}
-            <Player />
+            <Player name="Maurice" score={12}/>
+            <Player name="Kees" score={25}/>
+            <Player name="Piet" score={2}/>
         </div>
     )
 }
